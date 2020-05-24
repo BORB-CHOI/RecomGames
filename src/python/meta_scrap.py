f005a9a1850e03efd8d66f69ddd5694d0854809b
Your Name <you@example.com>
2020-05-25 00:53:40 +0900

    game dictionary

--- src/python/meta_scrap.py
+++ src/python/meta_scrap.py
@@ -23,6 +23,17 @@ def trans_genre(en_genre):
     return kr_genre
 
 
+def str_merge(strings):
+    merged_str = ""
+    for nth in range(len(strings)-1):
+        if nth > 0:
+            if strings[nth].string != strings[nth+1].string:
+                merged_str += f", {strings[nth+1].string}"
+        else:
+            merged_str += strings[nth+1].string
+    return merged_str
+
+
 def extract_gmae(game_url):
     response = requests.get(
         f"https://www.metacritic.com{game_url}", headers={"User-Agent": "Mozilla"})
@@ -32,16 +43,17 @@ def extract_gmae(game_url):
         "div", class_="product_title").a.get_text(strip=True)
     platform = game_info.find("div", class_="content_head").find(
         "div", class_="product_title").span.a.get_text(strip=True)
+    company = game_info.find("div", class_="content_head").find(
+        "div", class_="product_data").ul.find("li", class_="publisher").find("span", class_="data").a.get_text(strip=True)
     release_date = game_info.find("div", class_="content_head").find(
         "div", class_="product_data").ul.find("li", class_="release_data").find("span", class_="data").get_text(strip=True)
     main_img = game_info.find("div", class_="large_media").div.img["src"]
     genres = game_info.find("div", class_="summary_wrap").find("div", class_="section product_details").find(
         "div", class_="side_details").ul.find("li", class_="product_genre").find_all("span")
-    # release_date = trans_RD(release_date)
-    # genre = trans_genre(genre)
-    for genre in range(len(genres)-1):
-        print(genres[genre+1].string)
-    return {'title': title, 'releaseDate': release_date, 'platform': platform, 'mainImg': main_img, 'link': f"https://www.metacritic.com{game_url}"}
+
+    genres = str_merge(genres)
+
+    return print({'title': title, 'company': company, 'releaseDate': release_date, 'platform': platform, 'mainImg': main_img, 'genres': genres, 'link': f"https://www.metacritic.com{game_url}"})
 
 
 def extract_games(last_page, url):
